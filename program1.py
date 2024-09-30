7bd40645274e204eddb744431417d828c12e36f1
aayushicollegites <aayushi@collegites.tech>
2024-09-30 14:28:50 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+         stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
         pass
 
 
